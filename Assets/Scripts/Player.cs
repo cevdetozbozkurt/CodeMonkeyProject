6ecc8f36db3e141a70f41167ce1c8d683af516e5
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2023-02-22 17:08:56 +0300

    Create project, coding player controller, post processing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 7;
+
+    private void Update(){
+        Vector2 inputVector = new Vector2();
+
+        if(Input.GetKey(KeyCode.W)){
+            inputVector.y = +1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            inputVector.x = -1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            inputVector.y = -1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+        Debug.Log(inputVector);
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
