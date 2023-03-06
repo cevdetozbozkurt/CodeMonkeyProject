abbf22d5f9603383972fd99205216bae00ccf2ba
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2023-03-06 14:46:21 +0300

    Interact events

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,30 @@ public class Player : MonoBehaviour
     private bool isWalk;
     private Vector3 lastInteractDir;
 
+    private void Start(){
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDir != Vector3.zero){
+            lastInteractDir = moveDir;
+        }
+
+        float interactDisctance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDisctance, counterLayerMask)){
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }else{
+            Debug.Log("-");
+        }
+    }
+
     private void Update(){
         HandleMovement();
         HandleInteractions();
@@ -34,10 +58,10 @@ public class Player : MonoBehaviour
         if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDisctance, counterLayerMask)){
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
                 // Has ClearCounter
-                clearCounter.Interact();
+                
             }
         }else{
-            Debug.Log("-");
+            
         }
 
     }
